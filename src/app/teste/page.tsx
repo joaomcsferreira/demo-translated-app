b74d7e50415199c868bceafc7f0c36fb334a5fa8
joaomcsferreira <marcosc024@gmail.com>
2024-02-23 09:17:28 -0300

    fix: atualizar o type global para aceitar as propriedades do google

--- src/app/teste/page.tsx
+++ src/app/teste/page.tsx
@@ -3,19 +3,33 @@
 import React from "react";
 
 import GoogleTranslate from "@/components/GoogleTranslate";
+import Componente1 from "@/components/Componente1";
+import Componente2 from "@/components/Componente2";
+import Componente3 from "@/components/Componente3";
+import Componente4 from "@/components/Componente4";
 
 const Page = () => {
   return (
-    <>
+    <div
+      style={{
+        width: "40%",
+        margin: "auto",
+        display: "flex",
+        flexDirection: "column",
+        gap: "2rem",
+        alignItems: "center",
+        padding: "1rem",
+        textAlign: "justify",
+        textJustify: "inter-word",
+      }}
+    >
       <GoogleTranslate />
 
-      <div>
-        O sol brilha intensamente no céu azul, enquanto as folhas das árvores
-        balançam suavemente ao vento. É uma tarde tranquila de primavera, onde
-        os pássaros cantam e as flores desabrocham em cores vibrantes. É um
-        convite à contemplação e ao deleite da natureza.
-      </div>
-    </>
+      <Componente1 />
+      <Componente2 />
+      <Componente3 />
+      <Componente4 />
+    </div>
   );
 };
 
---
+++ src/components/Componente2.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Componente2 = () => {
+  return (
+    <div>
+      A lua lança seu brilho suave sobre o lago tranquilo, refletindo as
+      estrelas cintilantes acima. A noite está viva com sussurros do vento e o
+      suave farfalhar das folhas. É um momento de serenidade, onde o tempo
+      parece parar, abraçando a beleza tranquila do mundo noturno.
+    </div>
+  );
+};
+
+export default Componente2;
